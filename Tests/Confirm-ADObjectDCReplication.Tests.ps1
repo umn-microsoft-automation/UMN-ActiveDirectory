9ce0f3a91958371e865c2df47f48822514576204
Jeff Bolduan <jbolduan@umn.edu>
2019-06-20 12:41:26 -0500

    Create Confirm-ADObjectDCReplication.Tests.ps1

---
+++ Tests/Confirm-ADObjectDCReplication.Tests.ps1
@@ -0,0 +1,157 @@
+﻿$TestConfig = @{
+    "TestModuleName" = "UMN-ActiveDirectory"
+}
+
+try {
+    if ($ModuleRoot) {
+        Import-Module (Join-Path $ModuleRoot "$ModuleName.psd1") -Force
+    }
+    else {
+        if (Test-Path -Path "..\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1") {
+            Import-Module "..\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1" -Force
+        }
+        elseif (Test-Path -Path ".\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1") {
+            Import-Module ".\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1" -Force
+        }
+    }
+
+    # Module scope fixes problems with AD mocking.  It needs to be here to fix issues with running
+    # tests on devices without AD module installed.
+    InModuleScope -ModuleName $TestConfig.TestModuleName {
+        $TestPresetParams = @{
+            "DomainName" = "contoso.com"
+            "UserName"   = "TestUser"
+            "Group"      = "TestGroup"
+            "AddGroup"   = "TestLocalGroup"
+            "ComputerName" = "TestComputer"
+            "OU" = "OU=TestOU,DC=contoso,DC=com"
+        }
+
+        $FakeADUser = @{
+            "DistinguishedName" = "CN=$($TestPresetParams.UserName),CN=Users,DC=contoso,DC=com"
+            "Enabled"           = $true
+            "Name"              = $TestPresetParams.UserName
+            "SamAccountName"    = $TestPresetParams.UserName
+        }
+
+        $FakeADGroup = @{
+            "DistinguishedName" = "CN=$($TestPresetParams.Group),CN=Groups,DC=contoso,DC=com"
+            "Enabled"           = $true
+            "Name"              = $TestPresetParams.Group
+            "SamAccountName"    = $TestPresetParams.Group
+        }
+
+        $FakeADComputer = @{
+            "DistinguishedName" = "CN=$($TestPresetParams.ComputerName),CN=Computers,DC=contoso,DC=com"
+            "Enabled" = $true
+            "Name" = $TestPresetParams.ComputerName
+            "SamAccountName" = $TestPresetParams.ComputerName
+        }
+
+        function Get-ADDomainController {
+            [CmdletBinding()]
+            param(
+                [string]$Filter
+            )
+
+            [System.Collections.ArrayList]$ReturnObject = @()
+
+            foreach ($int in 1..6) {
+                $ReturnObject.Add([PSCustomObject]@{
+                        HostName = "DC$int"
+                    })
+            }
+
+            return $ReturnObject
+        }
+
+        Describe "Confirm-ADObjectDCReplication" {
+            Mock -CommandName Get-ADDomainController -MockWith {
+                [System.Collections.ArrayList]$ReturnObject = @()
+
+                foreach ($int in 1..6) {
+                    $ReturnObject.Add([PSCustomObject]@{
+                            HostName = "DC$int"
+                        })
+                }
+
+                return $ReturnObject
+            }
+
+            Context "Successful object replacation across domain controllers" {
+                Mock -CommandName Confirm-ADObjectExists { return $true }
+
+                It "Should return true if a computer is found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.ComputerName -Type "Computer" | Should -Be $true
+                }
+
+                It "Should return true if a user is found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.UserName -Type "User" | Should -Be $true
+                }
+
+                It "Should return true if a group is found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.Group -Type "Group" | Should -Be $true
+                }
+
+                It "Should return true if an OU is found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.OU -Type "OU" | Should -Be $true
+                }
+
+                It "Should return true if an unknown object is found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject "SomethingUnknown" -Type "Unknown" | Should -Be $true
+                }
+            }
+
+            Context "Unsuccessful object replacation across domain controllers" {
+                Mock -CommandName Confirm-ADObjectExists { return $false }
+
+                It "Should return false if a computer is not found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.ComputerName -Type "Computer" -MaxWait 2 | Should -Be $false
+                }
+
+                It "Should return false if a user is not found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.UserName -Type "User" -MaxWait 2 | Should -Be $false
+                }
+
+                It "Should return false if a group is not found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.Group -Type "Group" -MaxWait 2 | Should -Be $false
+                }
+
+                It "Should return false if an OU is not found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject $TestPresetParams.OU -Type "OU" -MaxWait 2 | Should -Be $false
+                }
+
+                It "Should return false if an unknown object is not found on all the domain controllers" {
+                    Confirm-ADObjectDCReplication -ADObject "SomethingUnknown" -Type "Unknown" -MaxWait 2 | Should -Be $false
+                }
+            }
+
+            Context "Error during main confirmation logic" {
+                Mock -CommandName Confirm-ADObjectExists { throw "error" }
+
+                It "Should throw an error if there is an error during the confirm logic for a computer" {
+                    { Confirm-ADObjectDCReplication -ADObject $TestPresetParams.ComputerName -Type "Computer" } | Should -Throw
+                }
+
+                It "Should throw an error if there is an error during the confirm logic for a user" {
+                    { Confirm-ADObjectDCReplication -ADObject $TestPresetParams.UserName -Type "User" } | Should -Throw
+                }
+
+                It "Should throw an error if there is an error during the confirm logic for a group" {
+                    { Confirm-ADObjectDCReplication -ADObject $TestPresetParams.Group -Type "Group" } | Should -Throw
+                }
+
+                It "Should throw an error if there is an error during the confirm logic for an OU" {
+                    { Confirm-ADObjectDCReplication -ADObject $TestPresetParams.OU -Type "OU" } | Should -Throw
+                }
+
+                It "Should throw an error if there is an error during the confirm logic for an unknown object" {
+                    { Confirm-ADObjectDCReplication -ADObject "SomethingUnknown" -Type "Unknown" } | Should -Throw
+                }
+            }
+        }
+    }
+}
+catch {
+    $Error[0]
+}
