9d2163d6dd917a190caefc0ee70b4367fe786ad6
Jeff Bolduan <jbolduan@umn.edu>
2019-06-21 15:25:20 -0500

    fully removing requirement until a better solution can be found
    This is still not working.

--- UMN-ActiveDirectory/UMN-ActiveDirectory.psm1
+++ UMN-ActiveDirectory/UMN-ActiveDirectory.psm1
@@ -1,7 +1,4 @@
-﻿# Use requires to fix bug when building nuget package in Azure DevOps
-#Requires -Modules ActiveDirectory
-
-$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+﻿$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 foreach( $Import in @($Public + $Private ) ) {
