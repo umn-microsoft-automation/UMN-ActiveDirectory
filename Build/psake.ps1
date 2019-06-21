160245cc7bcf1c11c08064ef2bbff63a02e2ba81
Jeff Bolduan <jbolduan@umn.edu>
2019-06-21 14:47:36 -0500

    added nuget build logic

--- Build/psake.ps1
+++ Build/psake.ps1
@@ -87,5 +87,71 @@ Task Test -Depends Init {
 }
 
 Task Build -Depends Test {
-    Update-Metadata -Path $env:BHPSModuleManifest -PropertyName "RequiredModules" -Value @("ActiveDirectory")
+    $Lines
+    Set-ModuleFunctions
+
+    if(-not (Test-Path -Path $env:GHBuildOutput)) {
+        New-Item $env:BHBuildOutput -Force -ItemType Directory
+    }
+
+    $AzureDevOpsCredentialPair = "$($env:PROJECT_PUBUSER):$($env:PROJECT_PUBKEY)"
+
+    $EncodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($AzureDevOpsCredentialPair))
+    $Headers = @{
+        "Authorization" = "Basic $EncodedCredentials"
+    }
+
+    try {
+        $DevOpsPackages = Invoke-RestMethod -Headers $Headers -Method Get -Uri "https://feeds.dev.azure.com/umn-microsoft/_apis/Packaging/Feeds/UMN-Internal/packages?api-version=5.0-preview.1"
+
+        $UMNActiveDirectoryInfo = $DevOpsPackages | Where-Object -FilterScript { $_.normalizedName -eq "UMN-ActiveDirectory" }
+
+        $UMNActiveDirectoryPkg = Invoke-RestMethod -Headers $Headers -Method Get -Uri "https://feeds.dev.azure.com/umn-microsoft/_apis/packaging/Feeds/UMN-Internal/packages/$($UMNActiveDirectoryInfo.id)?api-version=5.0-preview.1"
+
+        [System.Version]$AzureDevOpsVersion = [System.Version]$UMNActiveDirectoryPkg.value.versions.version
+
+        if($null -eq $AzureDevOpsVersion) {
+            Write-Warning -Message "Null Version Detected"
+            [System.Version]$AzureDevOpsVersion = "0.0.1"
+        }
+    } catch {
+        Write-Warning -Message "No DevOps version found or there were issues"
+        [System.Version]$AzureDevOpsVersion = "0.0.1"
+    }
+
+    try {
+        [System.Version]$GalleryVersion = Get-NextNugetPackageVersion -Name $env:GHProjectName -ErrorAction Stop
+    } catch {
+        Write-Warning -Message "Failed to update gallery version for '$env:BHProjectName': $_.`nContinuing with existing version"
+    }
+
+    try {
+        [System.Version]$GitHubVersion = Get-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -ErrorAction Stop
+    } catch {
+        Write-Warning -Message "Failed to update GitHub version for '$env:BHProjectName': $_`nContinuing with existing version"
+    }
+
+    Write-Host "---"
+    Write-Host "GalleryVersion = $($GalleryVersion.ToString())"
+    Write-Host "AzureDevOpsVersion = $($AzureDevOpsVersion.ToString())"
+    Write-Host "GitHubVersion = $($GitHubVersion.ToString())"
+    Write-Host "---"
+
+    if(($GalleryVersion -ge $GitHubVersion) -and ($GalleryVersion -ge $AzureDevOpsVersion)) {
+        $NewVersion = New-Object System.Version ($GalleryVersion.Major, $GalleryVersion.Minor, ($GalleryVersion.Build + 1))
+    } elseif($GitHubVersion -ge $AzureDevOpsVersion) {
+        $NewVersion = New-Object System.Version ($GitHubVersion.Major, $GitHubVersion.Minor, $GitHubVersion.Build)
+    } else {
+        $NewVersion = New-Object System.Version ($AzureDevOpsVersion.Major, $AzureDevOpsVersion.Minor, ($AzureDevOpsVersion.Build + 1))
+    }
+
+    Write-Warning -Message "NewVersion = $($NewVersion.ToString())"
+
+    Update-Metadata -Path $env:BHPSModuleManifest -PropertyName "ModuleVersion" -Value $NewVersion -ErrorAction Stop
+    Update-Metadata -Path $env:BHPSModuleManifest -PropertyName "RequiredModules" -Value @("ActiveDirectory") -ErrorAction Stop
+
+    Register-PSRepository -Name "LocalPSRepo" -SourceLocation "$env:BUILD_SOURCESDIRECTORY\BuildOutput" -PublishLocation "$env:BUILD_SOURCESDIRECTORY\BuildOutput" -InstallationPolicy Trusted
+    nuget.exe sources Add -Name "LocalPSRepo" -Source "$env:BUILD_SOURCESDIRECTORY\BuildOutput"
+
+    Publish-Module -Path $env:BHModulePath -Repository "LocalPSRepo" -NuGetApiKey "AzureDevOps" -Verbose
 }
