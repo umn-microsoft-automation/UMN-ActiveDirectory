c83899934b7a3fdd17176925ca3626f7bfd77e81
Jeff Bolduan <jbolduan@umn.edu>
2019-06-21 13:31:26 -0500

    Added basic test for Get-MembersLocalGroup

---
+++ Tests/Get-MembersLocalGroup.Tests.ps1
@@ -0,0 +1,41 @@
+﻿$TestConfig = @{
+    "TestModuleName" = "UMN-ActiveDirectory"
+}
+
+try {
+    if ($ModuleRoot) {
+        Import-Module (Join-Path $ModuleRoot "$ModuleName.psd1") -Force
+    }
+    else {
+        if (Test-Path -Path "..\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1") {
+            Import-Module "..\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1" -Force
+        }
+        elseif (Test-Path -Path ".\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1") {
+            Import-Module ".\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1" -Force
+        }
+    }
+
+    # Module scope fixes problems with AD mocking.  It needs to be here to fix issues with running
+    # tests on devices without AD module installed.
+    InModuleScope -ModuleName $TestConfig.TestModuleName {
+        if($ModuleRoot) {
+            . "$ModuleRoot..\Tests\StandardTestData.ps1"
+        } else {
+            if(Test-Path -Path "StandardTestData.ps1") {
+                . .\StandardTestData.ps1
+            } elseif(Test-Path -Path "Tests\StandardTestData.ps1") {
+                . .\Tests\StandardTestData.ps1
+            } else {
+                throw "Error importing StandardTestData.ps1"
+            }
+        }
+
+        Describe "Get-MembersLocalGroup" {
+            It "Should return the membership of the local admins group" {
+                Get-MembersLocalGroup -Group "Administrators" -Computer $env:COMPUTERNAME | Should -Not -Be $null
+            }
+        }
+    }
+} catch {
+    $Error[0]
+}
