769a2d9d07ae8fb3f66cb57ba2f9350e2ec4cc7f
Jeff Bolduan <jbolduan@gmail.com>
2020-03-19 10:16:43 -0500

    Fixed a whole host of bugs

--- Tests/UMN-ActiveDirectory.Help.Tests.ps1
+++ Tests/UMN-ActiveDirectory.Help.Tests.ps1
@@ -1,32 +1,48 @@
-﻿Import-Module (Join-Path $moduleRoot "$moduleName.psd1") -force
+﻿$TestConfig = @{
+    "TestModuleName" = "UMN-ActiveDirectory"
+}
 
-Describe "Help tests for $moduleName" -Tags 'Build' {
+try {
+    if ($ModuleRoot) {
+        Import-Module (Join-Path $ModuleRoot "$($TestConfig.TestModuleName).psd1") -Force
+    }
+    else {
+        if (Test-Path -Path ..\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1) {
+            Import-Module ..\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1 -Force    
+        }
+        elseif (Test-Path -Path .\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1) {
+            Import-Module .\$($TestConfig.TestModuleName)\$($TestConfig.TestModuleName).psd1 -Force
+        }
+    }
+    Describe "Help tests for $($TestConfig.TestModuleName)" -Tags 'Build' {
     
-    $functions = Get-Command -Module $moduleName -CommandType Function
-    foreach($Function in $Functions){
-        $help = Get-Help $Function.name
-        Context $help.name {
-            #it "Has a HelpUri" {
-            #    $Function.HelpUri | Should Not BeNullOrEmpty
-            #}
-            #It "Has related Links" {
-            #    $help.relatedLinks.navigationLink.uri.count | Should BeGreaterThan 0
-            #}
-            it "Has a description" {
-                $help.description | Should Not BeNullOrEmpty
-            }
-            it "Has an example" {
-                 $help.examples | Should Not BeNullOrEmpty
-            }
-            foreach($parameter in $help.parameters.parameter)
-            {
-                if($parameter -notmatch 'whatif|confirm')
-                {
-                    it "Has a Parameter description for '$($parameter.name)'" {
-                        $parameter.Description.text | Should Not BeNullOrEmpty
+        $functions = Get-Command -Module $($TestConfig.TestModuleName) -CommandType Function
+        foreach ($Function in $Functions) {
+            $help = Get-Help $Function.name
+            Context $help.name {
+                #it "Has a HelpUri" {
+                #    $Function.HelpUri | Should Not BeNullOrEmpty
+                #}
+                #It "Has related Links" {
+                #    $help.relatedLinks.navigationLink.uri.count | Should BeGreaterThan 0
+                #}
+                it "Has a description" {
+                    $help.description | Should Not BeNullOrEmpty
+                }
+                it "Has an example" {
+                    $help.examples | Should Not BeNullOrEmpty
+                }
+                foreach ($parameter in $help.parameters.parameter) {
+                    if ($parameter -notmatch 'whatif|confirm') {
+                        it "Has a Parameter description for '$($parameter.name)'" {
+                            $parameter.Description.text | Should Not BeNullOrEmpty
+                        }
                     }
                 }
             }
         }
     }
+}
+catch {
+    $Error[0]
 }
